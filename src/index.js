4a4530820b0928d3cb2c8211f95ffa98cdde0812
Thefenix2000 <m.wos.biznes@gmail.com>
2022-10-25 11:00:18 +0200

    Fix structure

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import "../node_modules/font-awesome/css/font-awesome.min.css"
 import "../node_modules/react-loading-skeleton/dist/skeleton.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  // </React.StrictMode> 
 );
